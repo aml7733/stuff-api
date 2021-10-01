341644b9c6eedcceda7698c6ebc2a0a03d706b01
aml7733 <lucia.adam@gmail.com>
2021-10-01 00:54:24 -0400

    add box_spec, gem shoulda-matchers

--- Gemfile
+++ Gemfile
@@ -35,8 +35,8 @@ gem 'responders'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
-
   gem 'rspec-rails', '~> 5.0.0'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- app/models/box.rb
+++ app/models/box.rb
@@ -3,7 +3,7 @@
 # Box model acts as both container and item for nested boxes
 class Box < ApplicationRecord
   validates :name, presence: true, uniqueness: true, allow_blank: false
-  validates :box_type, presence: true, allow_blank: true
+  validates :box_type, presence: true, allow_blank: false
   validates :description, presence: true
 
   has_many :children, class_name: 'Box', foreign_key: 'parent_id', dependent: :destroy, inverse_of: :parent
